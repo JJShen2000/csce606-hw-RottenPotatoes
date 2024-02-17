a04261f5d119c5f82eacb603bcee429269e865e0
JJShen2000 <gimgim7910@gmail.com>
2024-02-17 14:58:45 -0600

    chore(db): Add more movies data in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Castle in the Sky', :rating => 'G',
+  :release_date => '02-Aug-1986'},
+  {:title => 'From Up on Poppy Hill', :rating => 'G',
+  :release_date => '16-Jul-2011'},
+  {:title => 'The Wind Rises', :rating => 'G',
+  :release_date => '20-Jul-2013'}
 ]
 
 more_movies.each do |movie|
