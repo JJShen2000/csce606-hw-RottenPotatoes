8baa1cfa22e93316eeebb455df72250c96ed1f50
JJShen2000 <gimgim7910@gmail.com>
2024-02-17 15:01:31 -0600

    style: Run rubocop with autocorrect

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -10,22 +12,22 @@
 
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'},
-  {:title => 'Castle in the Sky', :rating => 'G',
-  :release_date => '02-Aug-1986'},
-  {:title => 'From Up on Poppy Hill', :rating => 'G',
-  :release_date => '16-Jul-2011'},
-  {:title => 'The Wind Rises', :rating => 'G',
-  :release_date => '20-Jul-2013'}
+  { title: 'My Neighbor Totoro', rating: 'G',
+    release_date: '16-Apr-1988' },
+  { title: 'Green Book', rating: 'PG-13',
+    release_date: '16-Nov-2018' },
+  { title: 'Parasite', rating: 'R',
+    release_date: '30-May-2019' },
+  { title: 'Nomadland', rating: 'R',
+    release_date: '19-Feb-2021' },
+  { title: 'CODA', rating: 'PG-13',
+    release_date: '13-Aug-2021' },
+  { title: 'Castle in the Sky', rating: 'G',
+    release_date: '02-Aug-1986' },
+  { title: 'From Up on Poppy Hill', rating: 'G',
+    release_date: '16-Jul-2011' },
+  { title: 'The Wind Rises', rating: 'G',
+    release_date: '20-Jul-2013' }
 ]
 
 more_movies.each do |movie|
